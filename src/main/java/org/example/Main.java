9e45869f7bd7a44bd2b15206e73842249654bf51
NicoMB2002 <nicomoncayo@hotmail.com>
2024-04-22 15:13:53 -0400

    Add a new println in the main()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a Toy Project");
+        System.out.println("Using Git Bash for VC");
     }
 }
 
 //git clone is the link that connects your local computer to repository
-//always do a pull toupdate your code
+//always do a pull to update your code
 //use git checkout to go back to previous versions
 //push every change to keep track of all modifications
